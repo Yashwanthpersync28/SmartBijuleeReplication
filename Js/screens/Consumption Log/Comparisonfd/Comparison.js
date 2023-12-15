c8f2ad3b6aa85cfc7fdb737ba16b10f5d4177e50
Perisync <dev@Perisyncs-MacBook-Pro.local>
2023-12-15 10:20:05 +0530

    sample

--- Js/screens/Consumption Log/Comparisonfd/Comparison.js
+++ Js/screens/Consumption Log/Comparisonfd/Comparison.js
@@ -1,62 +1,158 @@
-import React from 'react'
-import { TouchableOpacity,View,Text,ScrollView, StyleSheet,SafeAreaView, Dimensions } from 'react-native';
-import { borderColor, borderWidth, flex, fontSize, heightValue, lineHeight, marginPosition, padding, position, radius, styles, widthValue } from '../../../../styles/Styles';
+import React from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
+import {
+  fontSize,
+  heightValue,
+  marginPosition,
+  padding,
+  radius,
+  styles,
+  widthValue,
+} from '../../../../styles/Styles';
 import DrawerScreenWrapper from '../../Drawer/DrawerScreenWrapper';
 import SmallCards from './SmallCards';
 import MainCard from '../../CommonComponents/Cards/MainCard';
 import Menu from '../../CommonComponents/Cards/Menu';
 import Header from '../../CommonComponents/Cards/Header';
-import { weekly } from '../../CommonComponents/Cards/DataWeekly/DataMonthly';
+import {weekly} from '../../CommonComponents/Cards/DataWeekly/DataMonthly';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 const Comparison = ({navigation}) => {
-     let abc='<0 Units'   
-     let data=[{month:'November',earnings:5},{month:'December',earnings:6}]
-     let {height,width}=Dimensions.get('window') ;
-     let details="Compare Your daily, weekly & monthly consumption to keep a tab on electricity usage";
-     let heading="Unit";
-     let comp="Comparison"
+  let details =
+    'Compare Your daily, weekly & monthly consumption to keep a tab on electricity usage';
+  let heading = 'Unit';
+  let comp = 'Comparison';
   return (
-    
-    <DrawerScreenWrapper >
+    <DrawerScreenWrapper>
       <SafeAreaView>
-      <View style={[{height:heightValue(1),width:widthValue(1)},styles.bglightblack]}>
-         <Menu navigation={navigation}/>
-       <ScrollView showsVerticalScrollIndicator={false}>
-           <Header details={details} heading={heading} continueprop={comp} columnwise={false} display={false}/>
-          <View style={[styles.row,{height:heightValue(10),width:widthValue(1.1)},styles.spaceBetweenVertical,marginPosition('3%',0,0,'5%')]}>
-           <SmallCards day={'Today'} progress={true}/>
-           <SmallCards day={'Yesterday'} progress={false}/>
-         </View>
-        <MainCard name={"Weekly"} secondname={'Comparison'} showsingleline={false} data={weekly} xaxis={'day'} yaxis={'bill'}/>
-        <View style={[{height:heightValue(6),width:widthValue(1.1)},styles.bgdashblue,marginPosition(13,0,50,20),styles.column,radius(15),padding(15),styles.selfStart]}>
-             <View style={[styles.row]}>
-              <Text style={[styles.white,fontSize(19)]}>Monthly</Text>
-              <Text style={[styles.green,fontSize(19),marginPosition(0,0,0,5)]}>Comparison</Text>
-             </View>
-             <View style={[styles.column]}>
-                  <Text style={[styles.white,marginPosition(5,0,5,5),fontSize(12)]}>December</Text>
-                  <View style={[styles.row]}>
-                  <View style={[{backgroundColor:"#3c3c42",height:heightValue(60),width:widthValue(1.6)},radius(20),marginPosition(0,0,5)]}></View>
-                  <FontAwesome5Icon name='angle-left' style={[styles.green,fontSize(12),marginPosition(0,5,0,5)]}/>
-                  <Text style={[styles.green,fontSize(12),marginPosition(-2)]}>0 Units</Text>
-                  </View>
-                
-           </View>
-           <View style={[styles.column]}>
-               <Text style={[styles.white,marginPosition(0,0,5,5),fontSize(12)]}>November</Text>
-               <View style={[styles.row]}>
-                  <View style={[{backgroundColor:"#3c3c42",height:heightValue(60),width:widthValue(1.6)},radius(20),marginPosition(0,0,5)]}></View>
-                  <Text style={[styles.white,fontSize(12),marginPosition(0,0,0,5)]}>0 Units</Text>
-                  </View>
-          </View>
-    </View>
-    </ScrollView>
-</View>
-</SafeAreaView>
-</DrawerScreenWrapper>
+        <View
+          style={[
+            {height: heightValue(1), width: widthValue(1)},
+            styles.bglightblack,
+          ]}>
+          <Menu navigation={navigation} />
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Header
+              details={details}
+              heading={heading}
+              continueprop={comp}
+              columnwise={false}
+              display={false}
+            />
+            <View
+              style={[
+                styles.row,
+                {height: heightValue(10), width: widthValue(1.1)},
+                styles.spaceBetweenVertical,
+                marginPosition('3%', 0, 0, '5%'),
+              ]}>
+              <SmallCards day={'Today'} progress={true} />
+              <SmallCards day={'Yesterday'} progress={false} />
+            </View>
+            <MainCard
+              name={'Weekly'}
+              secondname={'Comparison'}
+              showsingleline={false}
+              data={weekly}
+              xaxis={'day'}
+              yaxis={'bill'}
+            />
+            <View
+              style={[
+                {height: heightValue(6), width: widthValue(1.1)},
+                styles.bgdashblue,
+                marginPosition(13, 0, 50, 20),
+                styles.column,
+                radius(15),
+                padding(15),
+                styles.selfStart,
+              ]}>
+              <View style={[styles.row]}>
+                <Text style={[styles.white, fontSize(19)]}>Monthly</Text>
+                <Text
+                  style={[
+                    styles.green,
+                    fontSize(19),
+                    marginPosition(0, 0, 0, 5),
+                  ]}>
+                  Comparison
+                </Text>
+              </View>
+              <View style={[styles.column]}>
+                <Text
+                  style={[
+                    styles.white,
+                    marginPosition(5, 0, 5, 5),
+                    fontSize(12),
+                  ]}>
+                  December
+                </Text>
+                <View style={[styles.row]}>
+                  <View
+                    style={[
+                      {
+                        backgroundColor: '#3c3c42',
+                        height: heightValue(60),
+                        width: widthValue(1.6),
+                      },
+                      radius(20),
+                      marginPosition(0, 0, 5),
+                    ]}></View>
+                  <FontAwesome5Icon
+                    name="angle-left"
+                    style={[
+                      styles.green,
+                      fontSize(12),
+                      marginPosition(0, 5, 0, 5),
+                    ]}
+                  />
+                  <Text
+                    style={[styles.green, fontSize(12), marginPosition(-2)]}>
+                    0 Units
+                  </Text>
+                </View>
+              </View>
+              <View style={[styles.column]}>
+                <Text
+                  style={[
+                    styles.white,
+                    marginPosition(0, 0, 5, 5),
+                    fontSize(12),
+                  ]}>
+                  November
+                </Text>
+                <View style={[styles.row]}>
+                  <View
+                    style={[
+                      {
+                        backgroundColor: '#3c3c42',
+                        height: heightValue(60),
+                        width: widthValue(1.6),
+                      },
+                      radius(20),
+                      marginPosition(0, 0, 5),
+                    ]}></View>
+                  <Text
+                    style={[
+                      styles.white,
+                      fontSize(12),
+                      marginPosition(0, 0, 0, 5),
+                    ]}>
+                    0 Units
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    </DrawerScreenWrapper>
+  );
+};
 
-  )
-}
-
-export default Comparison
+export default Comparison;
